7ea201ce947f2bbd70d68e46221a42575010bd81
Thorhian <thorhian@protonmail.com>
2019-09-25 21:31:11 -0600

    Removed some Debug code, wasn't using the right ID for text box.

--- web/teamAct1.js
+++ web/teamAct1.js
@@ -7,8 +7,6 @@ function clickAlert() {
 function changeTextColor() {
     colorID = document.getElementById("colorID").value;
     div = document.getElementById("section1");
-    div.innerHTML = "LOLOLOLOLOLOLOLOLOLOLOL";
     console.debug(colorID);
-    console.debug('green');
     div.style.backgroundColor = colorID;
 }
