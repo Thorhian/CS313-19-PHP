bdb91bbba5968f29057375fefbc2dba0b4ae305a
Thorhian <thorhian@protonmail.com>
2019-09-25 21:08:59 -0600

    Another function for color changes.

--- web/teamAct1.js
+++ web/teamAct1.js
@@ -3,3 +3,8 @@
 function clickAlert() {
     alert("Clicked!");
 }
+
+function changeTextColor() {
+    colorID = Document.getElementByID("changeColorButton").value;
+    Document.getElementByID("section1").color = colorID;
+}
