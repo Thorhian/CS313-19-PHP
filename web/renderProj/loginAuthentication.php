0a33e9090027268f8761100ad9ba1090614031f7
Thorhian <thorhian@protonmail.com>
2019-10-21 15:56:07 -0600

    Added initial folder and file for render management project.

---
+++ web/renderProj/loginAuthentication.php
@@ -0,0 +1,20 @@
+<?php
+require('db.php')
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Search Results for <?php echo $book; ?></title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+
+
+    </body>
+</html>
