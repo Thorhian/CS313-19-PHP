82c66efa7acb956c44a49721ae6f19f3d9525ef0
Thorhian <thorhian@protonmail.com>
2019-10-21 18:00:30 -0600

    Commenting out possibly unneeded HTML code here. May entirely remove later.

--- web/renderProj/loginAuthentication.php
+++ web/renderProj/loginAuthentication.php
@@ -3,12 +3,11 @@ require('db.php')
 
 
 ?>
-<!DOCTYPE html>
+{/* <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Search Results for <?php echo $book; ?></title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
         <link rel="stylesheet" href="style.css">
@@ -18,3 +17,4 @@ require('db.php')
 
     </body>
 </html>
+ */}
