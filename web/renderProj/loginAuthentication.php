4e4b71cb0b32d277350cc2c1c54f89f0fbf8938b
Thorhian <thorhian@protonmail.com>
2019-10-21 18:08:12 -0600

    Actually commenting code, oops.

--- web/renderProj/loginAuthentication.php
+++ web/renderProj/loginAuthentication.php
@@ -3,18 +3,19 @@ require('db.php')
 
 
 ?>
-{/* <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Search Results for <?php echo $book; ?></title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+<!--
+     <!DOCTYPE html>
+     <html lang="en">
+     <head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Search Results for <?php echo $book; ?></title>
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+     <link rel="stylesheet" href="style.css">
+     </head>
+     <body>
 
 
-    </body>
-</html>
- */}
+     </body>
+     </html>
+     //-->
