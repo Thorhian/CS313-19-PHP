8e4d062322a7afe78d0909de43c593fbf1b2e7da
Thorhian <thorhian@gmail.com>
2019-11-22 17:25:34 -0700

    Experiementing with navbar stuff

--- web/renderProj/login.php
+++ web/renderProj/login.php
@@ -23,7 +23,7 @@ include("generalFunctions.php")
         <![endif]-->
 
         <nav class="navbar navbar-fixed-top navbar-inverse">
-            <div class="container">
+            <div class="container-fluid">
                 <div class="navbar-header">
                     <a class="navbar-brand" href="#">Render Labs</a>
                 </div>
